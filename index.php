d0002e49827d8834d391945612c65c24f9f3e0bd
Mohammad AbuKaff <q_k_86@hotmail.com>
2021-01-16 15:31:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <script src="jquery.min.js" type="text/javascript"></script>
+        <link href="bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <style>
+            
+            #cars {
+    width: max-content;
+    height: 300px;
+    background-color: #333;
+    margin: 20px;
+    
+   
+
+    
+    overflow: visible;
+    white-space:nowrap;
+}
+
+#cars .slide {
+    display: inline-block;    
+    padding: 20px;
+  
+}
+#images{
+    width: 200px;
+    height: 200px;
+    
+}
+            
+            
+            
+
+</style>
+        <title></title>
+    </head>
+    <body>
+        <br><br><br><br>
+      <div id="cars">
+          <center>
+          <h1 style="color: white">Choose Category</h1>
+          </center>
+    <div class="slide">
+        <a href="">  <img id="images" src="Images/1.jpg" /> </a>
+    </div>
+    <div class="slide">
+        <a href="" > <img id="images" src="Images/2.jpg"  /></a>
+    </div>
+    <div class="slide">
+        <a href="" ><img id="images" src="Images/3.jpg"  /></a>
+    </div>
+    <div class="slide">
+        <a href=""> <img id="images" src="Images/4.jpg" /></a>
+    </div>
+    <div class="slide">
+        <a href="" > <img id="images" src="Images/5.jpg" /></a>
+    </div>
+    <div class="slide">
+        <a href="">  <img id="images" src="Images/6.jpg" /></a>
+    </div>
+          <div class="slide">
+              <a href="">  <img id="images" src="Images/6.jpg" /></a>
+    </div>
+          <div class="slide">
+              <a href="">  <img id="images" src="Images/6.jpg" /></a>
+    </div>
+          <div class="slide">
+              <a href="">  <img id="images" src="Images/6.jpg" /></a>
+    </div>
+          <div class="slide">
+              <a href="">  <img id="images" src="Images/6.jpg" /></a>
+    </div>
+          <div class="slide">
+              <a href="">  <img id="images" src="Images/6.jpg" /></a>
+    </div>
+</div>
+        
+        
+    </body>
+</html>
